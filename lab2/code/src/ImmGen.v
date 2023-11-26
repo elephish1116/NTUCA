d2860be59183343d2eb5a131b90a8986115f0d9b
elephish1116 <zxczxc11161116@gmail.com>
2023-11-26 16:19:45 +0800

    add R-type case in ImmGen

--- lab2/code/src/ImmGen.v
+++ lab2/code/src/ImmGen.v
@@ -8,6 +8,7 @@ input [31:0] instr_i;
 output [31:0] imm_o;
 
 assign imm_o = (instr_i[6:0] == 7'b0100011) ? {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]} :
-               (instr_i[6:0] == 7'b1100011) ? {{21{instr_i[31]}}, instr_i[7], instr_i[30:25], instr_i[11:8]} : {{20{instr_i[31]}}, instr_i[31:20]};
+               (instr_i[6:0] == 7'b1100011) ? {{21{instr_i[31]}}, instr_i[7], instr_i[30:25], instr_i[11:8]} :
+               (instr_i[6:0] == 7'b0110011) ? 32'b0 : {{20{instr_i[31]}}, instr_i[31:20]};
 
 endmodule
